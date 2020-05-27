2cfa9958012e00485f16002a211127061277a376
amjedidiah <imunacode@gmail.com>
2020-05-27 13:59:28 +0100

    feat: BrowserRouter routing works fine now

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/home/home.js';
+
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return <Router>
+    <Route path='/' component={Home} />
+  </Router>;
 }
 
 export default App;
