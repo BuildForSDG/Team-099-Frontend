03ad7139c95f66296c8c73bdaddca54318aede6a
amjedidiah <imunacode@gmail.com>
2020-06-01 11:53:40 +0100

    fix: Fixed Codacy errors

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import store from '../redux/store.js';
 
 // configure base url
 const api = axios.create({
-  baseURL: 'https://edukolab-api-team-099-b-li6nmp.herokuapp.com/v2'
+  baseURL: 'https://edukolab-api-team-099-b-li6nmp.herokuapp.com/v1'
 });
 
 // intercept requests and add authorization token
